2bfbe71f0330b52bc5e01d933c847d47970aef91
root <root@asterix.livinglab.chalmers.se>
2017-12-06 16:38:53 +0100

    final!

--- src/se/chalmers/hll/comet2mysql.java
+++ src/se/chalmers/hll/comet2mysql.java
@@ -6,9 +6,7 @@ package se.chalmers.hll;
 /**
  *
  * @author Alireza Davoudian
- * @date 04/12/2017 17:50 PM
- * @updated 20/10/2017 16:20
- * @updated 09/11/2017 18:40
+ * @date 06/12/2017 16:30 PM
  */
 import java.io.*;
 import java.net.MalformedURLException;
@@ -21,7 +19,6 @@ import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.w3c.dom.*;
 import javax.xml.parsers.*;
 import org.xml.sax.SAXException;
 
@@ -52,94 +49,8 @@ public class comet2mysql {
         }
 
         x2s.xml2mysql();
-
-        
-//        try {
-            //SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
-//            Class.forName("com.mysql.jdbc.Driver");
-//            Properties config = new Properties();
-//            InputStream input = null;
-//            input = new FileInputStream("/root/NetBeansProjects/Comet2mysql/src/resources/config.properties");
-//            config.load(input);
-//            String url = config.getProperty("url");
-//            String username = config.getProperty("username");
-//            String password = config.getProperty("password");
-//            Connection con = DriverManager.getConnection(url, username, password);
-//            Statement st = con.createStatement();
-//            DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-//            DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-//
-//            File folder = new File("/home/omnisense/");
-//            File[] listOfFiles = folder.listFiles();
-//            if (listOfFiles != null) {
-//                for (File child : listOfFiles) {
-//                    if (child.isFile() && child.getName().endsWith(".xml")) {
-//                        Document doc = docBuilder.parse(new File(child.getPath()));
-//                        doc.getDocumentElement().normalize();
-//                        System.out.println("Root element of the doc is " + doc.getDocumentElement().getNodeName());
-//                        NodeList listOfSensors = doc.getElementsByTagName("ut_sensor_reading");
-//                        for (int s = 0; s < listOfSensors.getLength(); s++) {
-//                            Node firstSensorNode = listOfSensors.item(s);
-//                            if (firstSensorNode.getNodeType() == Node.ELEMENT_NODE) {
-//                                Element firstElement = (Element) firstSensorNode;
-//                                String sensor_id = firstElement.getAttribute("sensor_id");
-//                                if (sensor_id.equals("1D300008")
-//                                        || sensor_id.equals("1D30000D")
-//                                        || sensor_id.equals("1D300072")
-//                                        || sensor_id.equals("1D3000B9")
-//                                        || sensor_id.equals("1D300111")
-//                                        || sensor_id.equals("1D3001AC")
-//                                        || sensor_id.equals("1D30021A")
-//                                        || sensor_id.equals("1D30028D")
-//                                        || sensor_id.equals("1D3002A7")
-//                                        || sensor_id.equals("1D300372")
-//                                        || sensor_id.equals("1D30038D")
-//                                        || sensor_id.equals("1D3003A3")
-//                                        || sensor_id.equals("1D3003CF")) {
-//                                    String time = firstElement.getAttribute("read_date");
-//                                    time = time.substring(0,time.length()-5);//time.replace(".000Z", "");
-//                                    //System.out.println(time);
-//                                    time = time.replace("T", " ");
-//                                    String temperature = firstElement.getAttribute("temperature");
-//                                    String humidity = firstElement.getAttribute("humidity");
-//                                    String wood_pct = firstElement.getAttribute("wood_pct");
-//                                    String gpp = firstElement.getAttribute("gpp");
-//                                    String dew_point = firstElement.getAttribute("dew_point");
-//                                    String sts_msg = firstElement.getAttribute("sts_msg");
-//                                    String time_added_to_mysql = LocalDateTime.now().toString();
-//                                    time_added_to_mysql= time_added_to_mysql.replace(".000Z", "");
-//                                    time_added_to_mysql = time_added_to_mysql.replace("T", " ");
-//                                    int i = st.executeUpdate("insert into omnisense(sensor_id,time,temperature,humidity,wood_pct,gpp,dew_point,msg,time_added_to_mysql) values('" + sensor_id
-//                                            + "','" + time + "','" + temperature + "','" + humidity + "','" + wood_pct + "','" + gpp + "','" + dew_point + "','" + sts_msg + "','" + time_added_to_mysql + "')");
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//            System.out.println("Data is successfully inserted!");
-//        } catch (IOException | ClassNotFoundException | SQLException | ParserConfigurationException | SAXException err) {
-//            System.out.println(" " + err.getMessage());
-//        }
     }
-//    private ArrayList ipList() {
-//        ArrayList<String> ip= new ArrayList<String>();
-//        try (BufferedReader br = new BufferedReader(new FileReader("/root/NetBeansProjects/Comet2mysql/src/resources/iplist"))){
-//            String sCurrentLine;
-//            while ((sCurrentLine= br.readLine())!=null){
-//                ip.add(sCurrentLine);
-//            }
-//        } catch (FileNotFoundException ex) {
-//            Logger.getLogger(comet2mysql.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (IOException ex) {
-//            Logger.getLogger(comet2mysql.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        
-//        return ip;
-//    }
 
-//    public void catchXML(){
-    // Using Java IO
     public static void saveXML(String fileName, String fileUrl) throws MalformedURLException, IOException {
         BufferedInputStream in = null;
         FileOutputStream fout = null;
--- src/se/chalmers/hll/xml2mysql.java
+++ src/se/chalmers/hll/xml2mysql.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package se.chalmers.hll;
 
 import java.io.File;
@@ -14,7 +9,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Properties;
 import javax.xml.parsers.DocumentBuilder;
@@ -28,13 +22,14 @@ import org.xml.sax.SAXException;
 
 /**
  *
- * @author root
+ * @author Alireza Davoudian
+ * @date 06/12/2017 16:30 PM
  */
 public class xml2mysql {
 
     public void xml2mysql() {
         try {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
             Class.forName("com.mysql.jdbc.Driver");
             Properties config = new Properties();
             InputStream input = null;
@@ -49,7 +44,7 @@ public class xml2mysql {
             DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
 
             Date date = new Date();
-            String timestamp = dateFormat.format(date).toString();
+            String timestamp = dateFormat.format(date);
             File folder = new File("/usr/local/bin/comet/temp");
             File[] listOfFiles = folder.listFiles();
             if (listOfFiles != null) {
@@ -59,135 +54,73 @@ public class xml2mysql {
                         doc.getDocumentElement().normalize();
                         System.out.println("Root element of the doc is " + doc.getDocumentElement().getNodeName());
                         NodeList xmlroot = doc.getElementsByTagName("root");
+                        NodeList err = doc.getElementsByTagName("err");
+                        NodeList serial_number = doc.getElementsByTagName("devsn");
+                        NodeList ch1 = doc.getElementsByTagName("ch1");
+                        NodeList ch2 = doc.getElementsByTagName("ch2");
+                        NodeList ch3 = doc.getElementsByTagName("ch3");
+                        NodeList ch4 = doc.getElementsByTagName("ch4");
+                        
+                        String error = err.item(0).getTextContent();
+                        String sn = serial_number.item(0).getTextContent();
+                        
+                        Node ch1Node = ch1.item(0);
+                        Element ch1Element = (Element) ch1Node;
+                        String channel_name = ch1Element.getElementsByTagName("name").item(0).getTextContent();
+                        String value = ch1Element.getElementsByTagName("aval").item(0).getTextContent();
+                        String unit = ch1Element.getElementsByTagName("unit").item(0).getTextContent();
+                        String alarm = ch1Element.getElementsByTagName("alarm").item(0).getTextContent();
+                        String channel_number = "1";
+                        int j = st.executeUpdate("insert into comet(timestamp,serial_number,error,"
+                                + " channel_number, channel_name, value, unit, alarm) "
+                                + "values('" + timestamp
+                                + "','" + sn + "','" + error + "','" + channel_number + "','" + channel_name + "','" + value
+                                + "','" + unit + "','" + alarm + "')");
 
-                        for (int s = 0; s < xmlroot.getLength(); s++) {
-                            Node firstNode = xmlroot.item(s);
-                            if (firstNode.getNodeType() == Node.ELEMENT_NODE) {
-                                Element firstElement = (Element) firstNode;
-                                String error = firstElement.getElementsByTagName("err").toString();
-                                String sn = firstElement.getElementsByTagName("devsn").toString();
-                                NodeList ch1 = firstElement.getElementsByTagName("ch1");
-                                for (int i = 0; i < ch1.getLength(); i++) {
-                                    Node firstCh1Node = ch1.item(i);
-                                    if (firstCh1Node.getNodeType() == Node.ELEMENT_NODE) {
-                                        Element firstCh1Element = (Element) firstCh1Node;
-                                        String channel_name = firstCh1Element.getElementsByTagName("name").toString();
-                                        String value = firstCh1Element.getElementsByTagName("aval").toString();
-                                        String unit = firstCh1Element.getElementsByTagName("\ufffdC").toString();
-                                        String alarm = firstCh1Element.getElementsByTagName("alarm").toString();
-                                        String channel_number = "1";
-                                        int j = st.executeUpdate("insert into comet(timestamp,serial_number,error,"
-                                                + " channel_number, channel_name, value, unit, alarm) "
-                                                + "values('" + timestamp
-                                                + "','" + sn + "','" + error + "','" + channel_number + "','" + channel_name + "','" + value
-                                                + "','" + unit + "','" + alarm + "')");
-                                    }
-                                }
-                                NodeList ch2 = firstElement.getElementsByTagName("ch2");
-                                for (int i = 0; i < ch2.getLength(); i++) {
-                                    Node firstCh2Node = ch2.item(i);
-                                    if (firstCh2Node.getNodeType() == Node.ELEMENT_NODE) {
-                                        Element firstCh2Element = (Element) firstCh2Node;
-                                        String channel_name = firstCh2Element.getElementsByTagName("name").toString();
-                                        String value = firstCh2Element.getElementsByTagName("aval").toString();
-                                        String unit = firstCh2Element.getElementsByTagName("%RH").toString();
-                                        String alarm = firstCh2Element.getElementsByTagName("alarm").toString();
-                                        String channel_number = "2";
-                                        int j = st.executeUpdate("insert into comet(timestamp,serial_number,error,"
-                                                + " channel_number, channel_name, value, unit, alarm) "
-                                                + "values('" + timestamp
-                                                + "','" + sn + "','" + error + "','" + channel_number + "','" + channel_name + "','" + value
-                                                + "','" + unit + "','" + alarm + "')");
-                                    }
-                                }
-                                NodeList ch3 = firstElement.getElementsByTagName("ch3");
-                                for (int i = 0; i < ch3.getLength(); i++) {
-                                    Node firstCh3Node = ch3.item(i);
-                                    if (firstCh3Node.getNodeType() == Node.ELEMENT_NODE) {
-                                        Element firstCh3Element = (Element) firstCh3Node;
-                                        String channel_name = firstCh3Element.getElementsByTagName("name").toString();
-                                        String value = firstCh3Element.getElementsByTagName("aval").toString();
-                                        String unit = firstCh3Element.getElementsByTagName("\ufffdC").toString();
-                                        String alarm = firstCh3Element.getElementsByTagName("alarm").toString();
-                                        String channel_number = "3";
-                                        int j = st.executeUpdate("insert into comet(timestamp,serial_number,error,"
-                                                + " channel_number, channel_name, value, unit, alarm) "
-                                                + "values('" + timestamp
-                                                + "','" + sn + "','" + error + "','" + channel_number + "','" + channel_name + "','" + value
-                                                + "','" + unit + "','" + alarm + "')");
-                                    }
-                                }
-                                NodeList ch4 = firstElement.getElementsByTagName("ch2");
-                                for (int i = 0; i < ch4.getLength(); i++) {
-                                    Node firstCh4Node = ch4.item(i);
-                                    if (firstCh4Node.getNodeType() == Node.ELEMENT_NODE) {
-                                        Element firstCh4Element = (Element) firstCh4Node;
-                                        String channel_name = firstCh4Element.getElementsByTagName("name").toString();
-                                        String value = firstCh4Element.getElementsByTagName("aval").toString();
-                                        String unit = firstCh4Element.getElementsByTagName("ppm").toString();
-                                        String alarm = firstCh4Element.getElementsByTagName("alarm").toString();
-                                        String channel_number = "4";
-                                        int j = st.executeUpdate("insert into comet(timestamp,serial_number,error,"
-                                                + " channel_number, channel_name, value, unit, alarm) "
-                                                + "values('" + timestamp
-                                                + "','" + sn + "','" + error + "','" + channel_number + "','" + channel_name + "','" + value
-                                                + "','" + unit + "','" + alarm + "')");
-                                    }
-                                }
-                            }
-                        }
+                        Node ch2Node = ch2.item(0);
+                        Element ch2Element = (Element) ch2Node;
+                        channel_name = ch2Element.getElementsByTagName("name").item(0).getTextContent();
+                        value = ch2Element.getElementsByTagName("aval").item(0).getTextContent();
+                        unit = ch2Element.getElementsByTagName("unit").item(0).getTextContent();
+                        alarm = ch2Element.getElementsByTagName("alarm").item(0).getTextContent();
+                        channel_number = "2";
+                        j = st.executeUpdate("insert into comet(timestamp,serial_number,error,"
+                                + " channel_number, channel_name, value, unit, alarm) "
+                                + "values('" + timestamp
+                                + "','" + sn + "','" + error + "','" + channel_number + "','" + channel_name + "','" + value
+                                + "','" + unit + "','" + alarm + "')");
+
+                        Node ch3Node = ch3.item(0);
+                        Element ch3Element = (Element) ch3Node;
+                        channel_name = ch3Element.getElementsByTagName("name").item(0).getTextContent();
+                        value = ch3Element.getElementsByTagName("aval").item(0).getTextContent();
+                        unit = ch3Element.getElementsByTagName("unit").item(0).getTextContent();
+                        alarm = ch3Element.getElementsByTagName("alarm").item(0).getTextContent();
+                        channel_number = "3";
+                        j = st.executeUpdate("insert into comet(timestamp,serial_number,error,"
+                                + " channel_number, channel_name, value, unit, alarm) "
+                                + "values('" + timestamp
+                                + "','" + sn + "','" + error + "','" + channel_number + "','" + channel_name + "','" + value
+                                + "','" + unit + "','" + alarm + "')");
+
+                        Node ch4Node = ch4.item(0);
+                        Element ch4Element = (Element) ch4Node;
+                        channel_name = ch4Element.getElementsByTagName("name").item(0).getTextContent();
+                        value = ch4Element.getElementsByTagName("aval").item(0).getTextContent();
+                        unit = ch4Element.getElementsByTagName("unit").item(0).getTextContent();
+                        alarm = ch4Element.getElementsByTagName("alarm").item(0).getTextContent();
+                        channel_number = "4";
+                        j = st.executeUpdate("insert into comet(timestamp,serial_number,error,"
+                                + " channel_number, channel_name, value, unit, alarm) "
+                                + "values('" + timestamp
+                                + "','" + sn + "','" + error + "','" + channel_number + "','" + channel_name + "','" + value
+                                + "','" + unit + "','" + alarm + "')");
                     }
                 }
             }
         } catch (IOException | ClassNotFoundException | SQLException | ParserConfigurationException | SAXException err) {
             System.out.println(" " + err.getMessage());
-
         }
-
-        /*  
-                        for (int s = 0; s < listOfSensors.getLength(); s++) {
-                            Node firstSensorNode = listOfSensors.item(s);
-                            if (firstSensorNode.getNodeType() == Node.ELEMENT_NODE) {
-                                Element firstElement = (Element) firstSensorNode;
-                                String sensor_id = firstElement.getAttribute("sensor_id");
-                                if (sensor_id.equals("1D300008")
-                                        || sensor_id.equals("1D30000D")
-                                        || sensor_id.equals("1D300072")
-                                        || sensor_id.equals("1D3000B9")
-                                        || sensor_id.equals("1D300111")
-                                        || sensor_id.equals("1D3001AC")
-                                        || sensor_id.equals("1D30021A")
-                                        || sensor_id.equals("1D30028D")
-                                        || sensor_id.equals("1D3002A7")
-                                        || sensor_id.equals("1D300372")
-                                        || sensor_id.equals("1D30038D")
-                                        || sensor_id.equals("1D3003A3")
-                                        || sensor_id.equals("1D3003CF")) {
-                                    String time = firstElement.getAttribute("read_date");
-                                    time = time.substring(0,time.length()-5);//time.replace(".000Z", "");
-                                    //System.out.println(time);
-                                    time = time.replace("T", " ");
-                                    String temperature = firstElement.getAttribute("temperature");
-                                    String humidity = firstElement.getAttribute("humidity");
-                                    String wood_pct = firstElement.getAttribute("wood_pct");
-                                    String gpp = firstElement.getAttribute("gpp");
-                                    String dew_point = firstElement.getAttribute("dew_point");
-                                    String sts_msg = firstElement.getAttribute("sts_msg");
-                                    String time_added_to_mysql = LocalDateTime.now().toString();
-                                    time_added_to_mysql= time_added_to_mysql.replace(".000Z", "");
-                                    time_added_to_mysql = time_added_to_mysql.replace("T", " ");
-                                    int i = st.executeUpdate("insert into omnisense(sensor_id,time,temperature,humidity,wood_pct,gpp,dew_point,msg,time_added_to_mysql) values('" + sensor_id
-                                            + "','" + time + "','" + temperature + "','" + humidity + "','" + wood_pct + "','" + gpp + "','" + dew_point + "','" + sts_msg + "','" + time_added_to_mysql + "')");
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            System.out.println("Data is successfully inserted!");
-        } catch (IOException | ClassNotFoundException | SQLException | ParserConfigurationException | SAXException err) {
-            System.out.println(" " + err.getMessage());
-        } */
     }
 
 }
